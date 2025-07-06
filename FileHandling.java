19e22502aa1643b58c6f0da123c7f6b778454474
shivrya <shivrajkhose4@gmail.com>
2025-07-05 17:00:43 -0700

    Java: File Handling

---
+++ FileHandling.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+
+public class FileHandling {
+
+    public static void main(String[] args) {
+
+        File file = new File("/Users/shivrajkhose/Documents/VSCode_Projects/Java/abc.txt");
+
+        file.delete();
+        
+    }
+
+}
