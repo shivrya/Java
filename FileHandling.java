2b88df3f8a22e96e1908ebe190064f3389380c9d
shivrya <shivrajkhose4@gmail.com>
2025-07-05 17:07:31 -0700

    Java File Handling

--- FileHandling.java
+++ FileHandling.java
@@ -7,7 +7,10 @@ public class FileHandling {
 
         File file = new File("/Users/shivrajkhose/Documents/VSCode_Projects/Java/abc.txt");
 
-        file.delete();
+        System.out.println(file.canRead());  // return true if we have the permissions to read, else returns false
+        System.out.println(file.canWrite()); // return true if we have the permissions to write, else returns false
+        System.out.println(file.length());   // returns length of the file in bytes
+        System.out.println(file.exists());   // returns true if file exists, else returns false
         
     }
 
